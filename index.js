403fcae2aea2c91b22d446a56341b18bea134b11
Pabloblockchain14 <pablo.arce.p@mail.pucv.cl>
2023-08-21 11:41:24 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,47 @@
+class ProductManager {
+    constructor(){
+        this.products = []
+    }
+
+    addProduct(title,description,price,thumbnail,code, stock){
+        const idProduct = this.products.length + 1
+        const codeEncontrado = this.products.find(producto => code===producto.code)
+        if (codeEncontrado){
+            console.log("Producto con code proporcionado ya existe")
+            return
+        }
+        if ( title =="" || description == "" || price == undefined || thumbnail=="" || code=="" || stock==undefined  ){
+            console.log("Faltan datos")
+            return
+        }
+        const producto = {
+            id: idProduct,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+        this.products.push(producto)
+    }
+    getProducts(){
+        return this.products
+    }
+    getProductById( idProduct ){
+        const productFound = this.products.find(producto => idProduct===producto.id)
+        productFound ? console.log(productFound) : console.error("NOT FOUND")
+    }
+}
+
+
+const productManager = new ProductManager()
+
+productManager.addProduct("Iphone 14" , "Iphone blanco 14 256GB" , 2500,  "./img/iphone14", "IP14" , 10)
+productManager.addProduct("Iphone 13" , "Iphone negro 13 128GB" , 1800,  "./img/iphone13","IP13", 5)
+productManager.addProduct("Iphone 12" , "Iphone rose 12 256GB" , 1500,   "./img/iphone12", "IP12", 15)
+
+console.log(productManager.getProducts())
+productManager.getProductById(5)
+
+
